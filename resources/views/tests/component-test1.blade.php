354a2006e906f163aa3ebc64c5da4976178abf92
sakata007 <be_best72@yahoo.co.jp>
2023-01-26 13:40:05 +0900

    css

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     コンポーネントテスト１
     <x-tests.card title="タイトル" content="本文" :message="$message"/>
     <x-tests.card title="タイトル２" />
+    <x-tests.card title="CSSを変更しない" class="bg-red-300"/>
 </x-tests.app>
