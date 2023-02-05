0107932a4f6e4acea51c0efcd4fb9976a5ab5e4d
sakata007 <be_best72@yahoo.co.jp>
2023-02-05 20:34:37 +0900

    ルーティング

--- routes/owner.php
+++ routes/owner.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +33,9 @@ middleware('auth:owners')->group(function() {
     Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
     Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)
+->middleware('auth:owners')->except('show');
 
 Route::get('/dashboard', function () {
     return view('owner.dashboard');
