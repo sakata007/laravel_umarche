9c051cf28f7aa6f951b105f9f7445985531e25f9
sakata007 <be_best72@yahoo.co.jp>
2023-02-05 17:18:23 +0900

    formrequest

---
+++ app/Http/Requests/UploadImageRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    // public function authorize()
+    // {
+    //     return true;
+    // }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'iamge' => 'image|mimes:jpg,jpeg,png|max;2048',
+            // 'image' => 'image|mimes:png|max:2048',
+        ];
+    }
+
+    public function authorize()
+    {
+        return [
+            'image' => '指定されたファイルが画像ではありません',
+            'mimes' => '指定した拡張子（jpg/jpeg/png）',
+            'max' => 'ファイルサイズは２MB以内にしてください',
+        ];
+    }
+}
