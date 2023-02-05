0552e684bddbb31cceb1b2aac412c1fedb834f6c
sakata007 <be_best72@yahoo.co.jp>
2023-02-05 13:21:34 +0900

    404

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use PgSql\Lob;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -31,6 +33,7 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+        Log::debug('owner', $request->session()->all());
 
         return redirect()->intended(RouteServiceProvider::OWNER_HOME);
     }
