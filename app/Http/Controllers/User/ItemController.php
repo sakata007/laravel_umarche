46f02eb6238afeb727a7ed4d6d03448fbb0fcbc6
sakata007 <be_best72@yahoo.co.jp>
2023-02-17 17:51:42 +0900

    user.show

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:users');
+    }
+
     public function index()
     {
         $stocks = DB::table('t_stocks')
@@ -36,8 +41,13 @@ class ItemController extends Controller
         ->get();
 
         // dd($stocks, $products);
-
         // $products = Product::all();
         return view('user.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return view('user.show', compact('product'));
     }
 }
