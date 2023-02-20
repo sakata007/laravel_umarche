72c806ddbf3cdb424332f624b1c4516333df79e5
sakata007 <be_best72@yahoo.co.jp>
2023-02-20 15:37:04 +0900

    カテゴリー検索実装

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -30,11 +31,17 @@ class ItemController extends Controller
 
     public function index(Request $request)
     {
+        // dd($request);
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
         $products = Product::availableItems()
+        // null合体演算子　nullだった時??の後の値を返す
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? 20);
 
-        return view('user.index', compact('products'));
+        return view('user.index', compact('products', 'categories'));
     }
 
     public function show($id)
